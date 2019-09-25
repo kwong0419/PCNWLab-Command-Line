72ee65707d22e7c21849b478a9968a39931563e1
Kevin Wong <kevinwong@pursuit.org>
2019-09-24 21:49:38 -0400

    Completed Probem 1 in pset1

--- pset1.js
+++ pset1.js
@@ -4,17 +4,29 @@ PROBLEM 1:
 Follow the following steps ONLY using your command line:
 
 1. Navigate to your Desktop directory.
+cd Desktop/
 2. Create a directory called my_first_direc.
+mkdir my_first_direc
 3. Enter the my_first_direc directory.
+cd my_first_direc
 4. Create a file called first.js.
+touch first.js
 5. Create a folder called assignments.
+mkdir assignments
 6. Move first.js into assignments.
+mv first.js assignments
 7. Go into the assignments folder.
+cd assignments
 8. Delete first.js.
+rm  first.js
 9. Go up to the my_first_direc directory.
+cd ..
 10. Delete the assignments folder.
+rm -r assignments
 11. Go up to the Desktop directory.
+cd ..
 12. Delete the test folder.
+rm -r my_first_direc
 
 ******************/
 
